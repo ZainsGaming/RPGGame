0f21291b095e7d3b173f3fb5e05700a665bbd4fa
szrrizvi <szrrizvi@ucalgary.ca>
2017-04-02 23:59:14 -0600

    Combat Initiative ordering.

--- src/zainsgaming/rpg/Driver.java
+++ src/zainsgaming/rpg/Driver.java
@@ -1,8 +1,9 @@
 package zainsgaming.rpg;
 
-import java.util.Random;
+import java.util.ArrayList;
 
 import zainsgaming.rpg.characters.ZGCharacter;
+import zainsgaming.rpg.events.CombatEvent;
 import zainsgaming.rpg.items.Weapon;
 
 public class Driver {
@@ -12,13 +13,35 @@ public class Driver {
 		
 		ZGCharacter p1 = new ZGCharacter("p1");
 		ZGCharacter p2 = new ZGCharacter("p2");
+		ZGCharacter p3 = new ZGCharacter("p3");
+		ZGCharacter p4 = new ZGCharacter("p4");
+		ZGCharacter p5 = new ZGCharacter("p5");
+		ZGCharacter p6 = new ZGCharacter("p6");
+		ZGCharacter p7 = new ZGCharacter("p7");
+		ZGCharacter p8 = new ZGCharacter("p8");
+		ZGCharacter p9 = new ZGCharacter("p9");
+		ZGCharacter p10 = new ZGCharacter("p10");
 		Weapon w1 = new Weapon(5);
 		Weapon w2 = new Weapon(4);
+		
+		ArrayList<ZGCharacter> list = new ArrayList<ZGCharacter>();
+		list.add(p1);
+		list.add(p2);
+		list.add(p3);
+		list.add(p4);
+		list.add(p5);
+		list.add(p6);
+		list.add(p7);
+		list.add(p8);
+		list.add(p9);
+		list.add(p10);
+		
+		CombatEvent ce = new CombatEvent(list);
 		
 		p1.setEquippedWeapon(w1);
 		p2.setEquippedWeapon(w2);
 		
-		
+		/*
 		Random rand = new Random(1234);
 		int p1Init = p1.rollInitiative();
 		int p2Init = p2.rollInitiative();
@@ -53,6 +76,6 @@ public class Driver {
 				}
 				
 			}
-		}
+		}*/
 	}
 }
--- src/zainsgaming/rpg/events/CombatEvent.java
+++ src/zainsgaming/rpg/events/CombatEvent.java
@@ -1,11 +1,79 @@
 package zainsgaming.rpg.events;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import zainsgaming.rpg.characters.ZGCharacter;
 
 public class CombatEvent extends Event {
 	private ArrayList<ZGCharacter> characters;
-	
-	
+	private ArrayList<ZGCharacter> initOrder;
+
+	public CombatEvent(ArrayList<ZGCharacter> chars){
+		characters = new ArrayList<ZGCharacter>();
+		characters.addAll(chars);
+		initOrder = new ArrayList<ZGCharacter>();
+		initializeCombat();
+	}
+
+	/**
+	 * Initialize the combat by rolling initiative and ordering the characters.
+	 */
+	private void initializeCombat(){
+		
+		//Roll the initiatives and store them in the map
+		Map<ZGCharacter, Integer> initMap = new HashMap<ZGCharacter, Integer>();
+		for (ZGCharacter zgChar : characters){
+			int initiative = zgChar.rollInitiative();
+			initMap.put(zgChar, initiative);
+		}
+
+		/*
+		for (ZGCharacter zgchar : initMap.keySet()){
+			System.out.println(zgchar.getName() + " " + initMap.get(zgchar));
+		}
+		System.out.println();
+		*/
+
+		// Add in initiative order the characters to initOrder
+		// Using insertion sort algorithm
+		for (ZGCharacter zgChar : characters){
+			//If initOrder is empty, then simply add the character as first element
+			if (initOrder.isEmpty()){
+				initOrder.add(zgChar);
+			} else {
+				//If the current character's initiative is greater than or equal to the first character in the order,
+				//Then set the current character as the first element. 
+				if (initMap.get(zgChar) >= initMap.get(initOrder.get(0))){
+					initOrder.add(0, zgChar);	//This method automatically shift the rest of the elements
+				} else {
+					boolean added = false;
+					//Loop through the order list, and add the first place where the current character belongs
+					for (int i = 1; i < initOrder.size(); i++){
+						if (initMap.get(zgChar) >= initMap.get(initOrder.get(i))){
+							initOrder.add(i, zgChar);
+							//Specify the character was added and end loop.
+							added = true;
+							i = initOrder.size();
+						}
+					}
+					
+					//If character was not added in the order during the for loop, that means that the character belongs
+					//at the end of the order
+					if (!added){
+						initOrder.add(zgChar);
+					}
+				}
+			}
+		}
+		
+		/*
+		for (ZGCharacter zgchar : initOrder){
+			System.out.println(zgchar.getName() + " " + initMap.get(zgchar));
+		}
+		*/
+
+	}
+
 }
